ae9a7e617c95c11ac8bf8dde16bf608ea82295c5
venkatkoolla <venkat.koolla2@gmail.com>
2016-08-03 15:26:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+<form action="<?php echo base_url('');?>" method="post" enctype="multipart/forn-data">
+		<div>
+		username:<input type="text" name="username">
+		</div>
+		<div>
+		<input type="submit" name="submit" value="Submit">
+		</div>
+	</form>
+
+</body>
+</html>
